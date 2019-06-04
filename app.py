a171f2baa60171e5dbc48bd1efb0c7a44fc9bead
Jenny Dang <qanhdang96@hotmail.com>
2019-06-04 18:22:32 -0500

    final

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template, redirect, send_file, jsonify
-from flask_pymongo import pymongo
 from functions import getRidOfId
-import time
-import tweets
 import os
 
 app = Flask(__name__)
 
-conn = "mongodb://localhost:27017/endgame_project"
-client = pymongo.MongoClient(conn)
 
 @app.route('/')
 def index():
@@ -22,18 +17,6 @@ def show_map():
 
     return send_file('templates/aus_map.html')
 
-@app.route("/grabtweets")
-def grabtweets():
-    tweets_info = client.db.tweets_info
-    tweets_data = tweets.scrape_tweets()
-    tweets_info.update({}, tweets_data, upsert=True)
-    return redirect("/", code=302)
-
-
-@app.route('/templates/tweets.html')
-def show_tweets():
-
-    return send_file("tweets.html")
 
 
 @app.route('/templates/entertainment.html')
